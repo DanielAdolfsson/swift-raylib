bf75360a659a9ba8a328b4c81235bbd011066777
Daniel Adolfsson <daniel@ashen.se>
2025-07-09 05:37:54 +0800

    Add Wayland and X11 traits

--- Package.swift
+++ Package.swift
@@ -7,8 +7,20 @@ let package = Package(
 	products: [
 		.library(name: "RaylibKit", targets: ["RaylibKit"]),
 	],
+	traits: [
+		.default(enabledTraits: ["X11"]),
+		"X11",
+		"Wayland",
+	],
 	dependencies: [
-		.package(url: "https://github.com/Lancelotbronner/raylib-build.git", from: "5.5.2"),
+		.package(
+			url: "https://github.com/Lancelotbronner/raylib-build.git",
+			from: "5.5.2",
+			traits: [
+				.trait(name: "Wayland", condition: .when(traits: ["Wayland"])),
+				.trait(name: "X11", condition: .when(traits: ["X11"])),
+			],
+		)
 	],
 	targets: [
 		.target(name: "RaylibKit", dependencies: [
